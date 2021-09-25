304ed366cc469f1983fdaf3e1f5c5a830d9e3d41
Paulo Gustavo <paulo.lata@aticks.com>
2021-09-25 15:59:39 -0300

    refactor(home): add div home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,6 +2,18 @@ import React, { FunctionComponent } from 'react';
 
 export const Home: FunctionComponent = () => {
   return (
-    <p> Texto simples e inicial. </p>
+    <div
+      style={{
+        display: 'flex',
+        width: '100vw',
+        height: '100vh',
+        background: '#6C7A89',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+    >
+      <p> Texto simples e inicial. </p>
+    </div>
   );
 }
