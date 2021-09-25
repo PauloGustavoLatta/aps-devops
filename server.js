0d33f2a70f4c82f75db5e43899eeda10a7c5d9df
Paulo Gustavo <paulo.lata@aticks.com>
2021-09-25 14:31:33 -0300

    refactor(server): change import server

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-import express from 'express';
-import { resolve } from 'path';
+const express = require('express');
+const { resolve } = require('path');
+
+const app = express();
 
 app.use('/',
   express.static(
@@ -8,12 +10,12 @@ app.use('/',
       './dist'
     )
   )
-)
+);
 
-app.listen(process.env.PORT ||  3000, (err) => {
-  if(err) {
-    return console.log(err)
-  };
-
-  console.log('Tudo funcionando como deveria.')
+app.listen(process.env.PORT || 3000, (err) => {
+  if (err) {
+    return console.log(err);
+  } else {
+    console.log('Tudo funcionando');
+  }
 });
